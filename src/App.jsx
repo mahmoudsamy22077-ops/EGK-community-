7e8314942d119fd1aa3e200d20532ffcf28d4258
mahmoudsamy22077-ops <mahmoudsamy22077@gmail.com>
2026-09-03 21:58:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EntryPage from './pages/EntryPage'
 import HomePage from './pages/HomePage'
+import AdminPage from './pages/AdminPage'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<EntryPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   )
